7475e259484cba3e5dfbb21a6b0151a11bf26218
vadym4che <vadym4che@gmail.com>
2023-05-10 11:54:34 +0300

    Implement dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.scss';
+import { DropDown } from './components/DropDown';
 
 export const App: React.FC = () => {
-  return <h1>Product catalog app</h1>;
+  const sortByList = ['Newest', 'Popular', 'Color'];
+  const itemsOnPageList = ['4', '6', '8', '16'];
+  const [selectedSortBy, setSelectedSortBy] = useState(sortByList[0]);
+  const [itemsOnPage, setItemsOnPage] = useState(itemsOnPageList[3]);
+
+  return (
+    <>
+      <h1>Product catalog app</h1>
+
+      <div className="inlineBlock width11rem">
+        <DropDown
+          optionList={sortByList}
+          selectedOption={selectedSortBy}
+          setSelectedOption={setSelectedSortBy}
+          description={'Sort by'}
+        />
+      </div>
+
+      <div className="inlineBlock width8rem">
+        <DropDown
+          optionList={itemsOnPageList}
+          selectedOption={itemsOnPage}
+          setSelectedOption={setItemsOnPage}
+          description={'Items on page'}
+        />
+      </div>
+    </>
+  );
 };
