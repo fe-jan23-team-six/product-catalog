ce3ca06dca38fddc51cfd028dfca8aa88addf928
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-05-11 12:07:55 +0300

    add initial device details page layout and some scss code

---
+++ src/pages/DeviceDetailsPage/DeviceDetailsSpecs/DeviceDetailsSpecs.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import './DeviceDetailsSpecs.scss';
+
+export const DeviceDetailsSpecs: FC = () => {
+  return (
+    <div
+      className="device_details__specs specs
+      grid__item--tablet-7-12 grid__item--desktop-14-24"
+    >
+      <h3 className="specs__title">
+        Tech Specs
+      </h3>
+
+      <div className="specs__characteristics characteristics">
+          <p
+            className="characteristics__text"
+          >
+            Screen
+          </p>
+
+          <p
+            className="characteristics__text characteristics__text--black"
+          >
+            6.5” OLED
+          </p>
+        </div>
+    </div>
+  );
+};
