d2c9ed0d977871ccfe95368504e614912c52de71
gretoq <dovhun3d@gmail.com>
2023-05-13 00:28:52 +0300

    Create SliderPhoto, but have some problems with table and phone view

--- src/components/SliderPhotos/SliderPhotos.tsx
+++ src/components/SliderPhotos/SliderPhotos.tsx
@@ -22,25 +22,24 @@ export const SliderPhotos: React.FC<Props> = ({ images }) => {
   return (
     <div className="slider-photos">
       <Swiper
-        spaceBetween={10}
         thumbs={{
-          swiper: thumbsSwiper
-            && !thumbsSwiper.destroyed
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed
             ? thumbsSwiper
             : null,
         }}
         modules={[FreeMode, Navigation, Thumbs]}
-        className="slider-photos"
+        className="slider-photos__main-photo-container"
       >
         {images.map(slide => {
           const { image } = slide;
+          // do destruct on slide after deleting global
 
-          global.console.log('image:', image);
+          // global.console.log('image:', image);
 
           return (
             <SwiperSlide key={image}>
               <img
-                className="photo-slider__image"
+                className="slider-photos__main-photo-container__image"
                 src={image}
                 alt={image}
               />
@@ -48,24 +47,38 @@ export const SliderPhotos: React.FC<Props> = ({ images }) => {
           );
         })}
       </Swiper>
+
       <Swiper
-      onSwiper={setThumbsSwiper}
-      spaceBetween={10}
-      slidesPerView={5}
-      freeMode
-      watchSlidesProgress
-      modules={[FreeMode, Navigation, Thumbs]}
-      className="mySwiper"
+        onSwiper={setThumbsSwiper}
+        spaceBetween={8}
+        slidesPerView={5}
+        freeMode
+        watchSlidesProgress
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="slider-photos__gallry-container"
+        direction='horizontal' // 'horizontal'
+        autoHeight
+        breakpoints={{
+          639: {
+            direction: 'vertical',
+            spaceBetween: 0,
+          },
+          1199: {
+            spaceBetween: 16,
+            direction: 'vertical',
+          },
+        }}
       >
         {images.map(slide => {
           const { image } = slide;
+          // do destruct on slide after deleting global
 
-          global.console.log('image:', image);
+          // global.console.log('image:', image);
 
           return (
             <SwiperSlide key={image}>
               <img
-                className="photo-slider__image"
+                className="slider-photos__gallry-container__image"
                 src={image}
                 alt={image}
               />
@@ -76,96 +89,3 @@ export const SliderPhotos: React.FC<Props> = ({ images }) => {
     </div>
   );
 };
-
-// import React, { useRef } from 'react';
-// import Slider, { Settings } from 'react-slick';
-// // import { ArrowButton } from '../ArrowButton';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-// import './SliderPhotos.scss';
-// import { SliderPhotosType } from '../../pages/HomePage';
-
-// interface Props {
-//   images: SliderPhotosType[],
-// }
-
-// export const SliderPhotos: React.FC<Props> = ({ images }) => {
-//   const sliderRef = useRef<Slider>(null);
-
-//   const imgBaseUrl = images[0].image
-//     .split('-')
-//     .slice(0, images[0].image.split('-').length - 1)
-//     .join('-');
-
-//   global.console.log('imgBaseUrl:', imgBaseUrl);
-
-//   const settings: Settings = {
-//     dotsClass: 'slick-dots slick-thumb', // photo-slider__pagination',
-//     dots: true,
-//     arrows: false,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     customPaging: function(i) {
-//       global.console.log('created', `${imgBaseUrl}-${i + 1}.png`);
-
-//       return (
-//         <a
-//           className='photo-slider__paging'
-//         >
-// <img src={`${imgBaseUrl}-${i + 1}.png`} alt={`${imgBaseUrl}-${i + 1}.png`} />
-//         </a>
-//       );
-//     },
-//   };
-
-//   // const handlePrevClick = () => {
-//   //   sliderRef.current?.slickPrev();
-//   // };
-
-//   // const handleNextClick = () => {
-//   //   sliderRef.current?.slickNext();
-//   // };
-
-//   return (
-//     <div className="photo-slider">
-
-//     {/* <div className="photo-slider__button-arrow">
-//       <ArrowButton
-//         classes="photo-slider__button"
-//         action={handlePrevClick}
-//       />
-//     </div> */}
-
-//       <Slider
-//         ref={sliderRef}
-//         className="photo-slider__container"
-//         {...settings}
-//       >
-
-//         {images.map(slide => {
-//           const { id, image } = slide;
-
-//           global.console.log('image:', image);
-
-//           return (
-//             <img
-//               className="photo-slider__image"
-//               src={image}
-//               alt={image}
-//               key={id}
-//             />
-//           );
-//         })}
-//       </Slider>
-
-//       {/* <div className="photo-slider__button-arrow">
-//         <ArrowButton
-//           classes="forward test photo-slider__button"
-//           action={handleNextClick}
-//         />
-//       </div> */}
-//     </div>
-//   );
-// };
