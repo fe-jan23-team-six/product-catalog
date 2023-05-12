0c82f712abef9847afb4e632e3efc8753dae38a3
Anna <arkushenko98@gmail.com>
2023-05-12 17:10:52 +0300

    Footer

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BurgerMenu.scss';
+
+export const BurgerMenu: React.FC = () => {
+  return (
+    <h1>BurgerMenu</h1>
+  );
+};
--- src/pages/FavouritesPage/FavouritePage.tsx
+++ src/pages/FavouritesPage/FavouritePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './FavouritePage.scss';
 import { ProductCard } from '../../components/ProductCard/ProductCard';
+import { BurgerMenu } from '../../components/BurgerMenu/BurgerMenu';
 
 export const FavouritePage: React.FC = () => {
   return (
     <>
       <h1>Favourite Page</h1>
+      <BurgerMenu />
       <ProductCard />
     </>
   );
