2557cd25d42459f9665213692825aa9d08ce7617
gretoq <dovhun3d@gmail.com>
2023-05-13 18:55:15 +0300

    Change pc

--- src/components/ShopByCategory/ShopByCategory.tsx
+++ src/components/ShopByCategory/ShopByCategory.tsx
@@ -7,24 +7,66 @@ export const ShopByCategory: React.FC = () => {
     <div className="shop-by-category">
       <h2>Shop by category</h2>
 
-      <div className="shop-by-category__catgory-container">
-        <Link to={'/catalog'}>
-          <div className="category-container__img-container">
-            <img src="img/shop-by-category/phones.png" alt="phone" />
-          </div>
-        </Link>
-        <h3></h3>
-        <p></p>
-      </div>
-      <div className="shop-by-category__catgory-container">
-        <a href=""><img src="" alt="" /></a>
-        <h3></h3>
-        <p></p>
-      </div>
-      <div className="shop-by-category__catgory-container">
-        <a href=""><img src="" alt="" /></a>
-        <h3></h3>
-        <p></p>
+      <div className="shop-by-category__categories grid">
+        <div className="categories__container">
+          <Link to={'/catalog'}>
+            <div className="categories__image-container">
+              <img
+                className="categories__image"
+                src="img/shop-by-category/phones.png"
+                alt="phone"
+              />
+            </div>
+          </Link>
+
+          <h3 className="categories__title">
+            Mobile phones
+          </h3>
+
+          <p className="categories__description">
+            95 models
+          </p>
+        </div>
+
+        <div className="categories__container">
+          <Link to={'/catalog'}>
+            <div className="categories__image-container">
+              <img
+                className="categories__image"
+                src="img/shop-by-category/phones.png"
+                alt="phone"
+              />
+            </div>
+          </Link>
+
+          <h3 className="categories__title">
+            Mobile phones
+          </h3>
+
+          <p className="categories__description">
+            95 models
+          </p>
+        </div>
+
+        <div className="categories__container">
+          <Link to={'/catalog'}>
+            <div className="categories__image-container">
+              <img
+                className="categories__image"
+                src="img/shop-by-category/phones.png"
+                alt="phone"
+              />
+            </div>
+          </Link>
+
+          <h3 className="categories__title">
+            Mobile phones
+          </h3>
+
+          <p className="categories__description">
+            95 models
+          </p>
+        </div>
       </div>
     </div>
   );
