c33541a44ebc3986ee7c238aaaea9ba67d270e83
Sergey Lipikhin <football3@ukr.net>
2023-05-15 01:18:18 +0300

    Add dropdowns to Catalog Page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -3,6 +3,7 @@ import './CatalogPage.scss';
 import { PaginationPannel } from '../../components/PaginationPannel';
 import { ProductList } from '../../components/ProductList';
 import { DataLoader } from '../../components/DataLoader';
+import { DropDown } from '../../components/DropDown';
 
 import { useDataFetcher } from '../../hooks/useDataFetcher';
 import { PhoneMain } from '../../types/phone/PhoneMain';
@@ -11,6 +12,9 @@ import { getPhones } from '../../utils/api/phones';
 export const CatalogPage: React.FC = () => {
   const [products, setProducts] = useState<PhoneMain[]>([]);
   const [productsFetchStatus, fetchProducts] = useDataFetcher();
+
+  const [selectedOption, setSelectedOption] = useState('Green');
+  const optionList = ['Red', 'Blue', 'Green', 'Yellow'];
 
   useEffect(() => {
     fetchProducts(() => getPhones().then(setProducts));
@@ -26,6 +30,30 @@ export const CatalogPage: React.FC = () => {
         className="bodyText catalog-page__subtitle"
       >
         95 models
+      </section>
+
+      <section className="catalog-page__dropdown-block">
+        <div
+          className="catalog-page__dropdown-left catalog-page__dropdown-item"
+        >
+          <DropDown
+            optionList={optionList}
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            description='Something'
+          />
+        </div>
+
+        <div
+          className="catalog-page__dropdown-right catalog-page__dropdown-item"
+        >
+          <DropDown
+            optionList={optionList}
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            description='Something'
+          />
+        </div>
       </section>
 
       <section className="catalog-page__product-list">
