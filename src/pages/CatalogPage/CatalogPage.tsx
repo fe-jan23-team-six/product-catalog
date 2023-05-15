96dee5ea4b62057fbd68b3280c506acf0515764a
Sergey Lipikhin <football3@ukr.net>
2023-05-16 02:43:29 +0300

    Use custom dropdown

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CatalogPage.scss';
 import { PaginationPannel } from '../../components/PaginationPannel';
 import { ProductList } from '../../components/ProductList';
@@ -11,12 +11,32 @@ import { useSearchParams } from 'react-router-dom';
 
 import { useDidUpdateEffect } from '../../hooks/useDidUpdateEffect';
 
+import { DropDown } from '../../components/DropDown';
+
 export const CatalogPage: React.FC = () => {
   const [products, setProducts] = useState<PhoneMain[]>([]);
   const [totalAmount, setTotalAmount] = useState('');
   const [fetchProductsAndAmountStatus, , fetchAll] = useDataFetcher();
 
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const [currentSort, setCurrentSort] = useState(
+    searchParams.get('sort') || 'alphabetically',
+  );
+  const [currentPerPage, setCurrentPerPage] = useState(
+    searchParams.get('limit') || '16',
+  );
+
+  useEffect(() => {
+    searchParams.set('sort', currentSort.toLowerCase());
+    setSearchParams(searchParams);
+  }, [currentSort]);
+
+  useEffect(() => {
+    searchParams.set('limit', currentPerPage.toLowerCase());
+    setSearchParams(searchParams);
+  }, [currentPerPage]);
+
   const page = searchParams.get('page') || '1';
   const limit = searchParams.get('limit') || '16';
   const sort = searchParams.get('sort') || 'alphabetically';
@@ -97,13 +117,25 @@ export const CatalogPage: React.FC = () => {
                 <option value="alphabetically">Alphabetically</option>
                 <option value="price-lowest">Cheapest</option>
               </select>
+
+              <DropDown
+                optionList={['4', '8', '16']}
+                selectedOption={currentPerPage}
+                setSelectedOption={setCurrentPerPage}
+                description='Something'
+              />
             </div>
 
             <div
               className="catalog-page__dropdown-right
               catalog-page__dropdown-item"
             >
-            {/* Custom dropdown was here */}
+              <DropDown
+                optionList={['newest', 'alphabetically', 'price-lowest']}
+                selectedOption={currentSort}
+                setSelectedOption={setCurrentSort}
+                description='Something'
+              />
             </div>
           </section>
 
